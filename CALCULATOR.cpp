1e80047cf2825b3ca41a795bc70e20257ab57e53
notaddtry <33911781+notaddtry@users.noreply.github.com>
2018-02-19 14:42:03 +0300

    Update CALCULATOR.cpp

--- CALCULATOR.cpp
+++ CALCULATOR.cpp
@@ -1,6 +1,7 @@
-#include"Calculator.h"
-void printresult(double r) {
-	cout << r << endl;
+#include "Calculator.h"
+void printresult(double r){
+	cout << r<< endl;
+
 }
 double addx(double a, double b) {
 	return a + b;
@@ -8,18 +9,19 @@ double addx(double a, double b) {
 double addy(double c, double d) {
 	return c + d;
 }
+double multiplyx(double a, double n) {
+	return a * n;
+}
+double multiplyy(double c, double n) {
+	return c * n;
+}
 double substractx(double a, double b) {
 	return a - b;
 }
 double substracty(double c, double d) {
 	return c - d;
 }
-double multiplyx(double a,  double n) {
-	return a * n;
-}
-double multiplyy(double c, double n) {
-	return c * n;
-}
 double stray(double a, double b, double c, double d) {
-	return a * b + c + d;
+	return a * b + c * d;
 }
+
